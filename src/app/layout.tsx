a8f45203403917deda39077b3f209745a9be570b
Lakshay Bansal <lakshaybansal@Lakshays-MacBook-Air.local>
2024-10-18 02:01:12 +0530

    Google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/react"
+/*import { Analytics } from "@vercel/analytics/react"
 import { Sidebar } from "@/components/Sidebar";
 import "./globals.css";
 import type { Metadata } from "next";
@@ -37,7 +37,76 @@ export default function RootLayout({
             <Footer />
           </div>
         </div>
-        {/* Add the Analytics component here */}
+        {/* Add the Analytics component here }
+        <Analytics />
+      </body>
+    </html>
+  );
+}
+  */
+
+// -----------------------------------------------------------------
+
+import { Analytics } from "@vercel/analytics/react";
+import { Sidebar } from "@/components/Sidebar";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { twMerge } from "tailwind-merge";
+import { Footer } from "@/components/Footer";
+import Script from "next/script";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Lakshay Bansal - Developer",
+  description:
+    "Lakshay Bansal is a developer, writer, and speaker. He is a digital nomad and travels around the world while working remotely.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(
+          inter.className,
+          "flex antialiased h-screen overflow-hidden bg-gray-100"
+        )}
+      >
+        {/* Google Analytics Script with environment variable */}
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+            `,
+          }}
+        />
+
+        <Sidebar />
+        <div className="lg:pl-2 lg:pt-2 bg-gray-100 flex-1 overflow-y-auto">
+          <div className="flex-1 bg-white min-h-screen lg:rounded-tl-xl border border-transparent lg:border-neutral-200 overflow-y-auto">
+            {children}
+            <Footer />
+          </div>
+        </div>
+
+        {/* Vercel Analytics */}
         <Analytics />
       </body>
     </html>
